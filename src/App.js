610ddfd5fc63e1fd1577210cb942f20564407e84
Zoya <zosi71@mail.ru>
2023-03-07 10:39:11 +0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -5,71 +5,111 @@ import Details from './components/Details';
 
 function App() {
   const [data, SetData] = useState([]);
+  const [loading, SetLoading] = useState(false);
   const [objView, SetObjView] = useState({
-    id: '',    
+    id: '',
     objdata: '',
   })
-  const url = 'https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json';  
-  
+  const url = 'https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json';
+
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(url);
       if (!response.ok) { throw new Error(response.statusText) }
       const data = await response.json();
-      console.log('from useEffec data=', data);
-      SetData(data)
+      let newdata = data.map((item) => {
+        return { ...item, active: false };
+      })
+      SetData(newdata)
     }
     try {
-      fetchData();
+      setTimeout(fetchData(), 3000);
     }
     catch (e) { console.error(e) }
   }, [])
 
-
   useEffect(() => {
+    
     async function fetchData() {
-      let id = objView.id;
-      let url = `https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${id}.json`;
-      const response = await fetch(url);
-      if (!response.ok) { throw new Error(response.statusText) }
-      const objforview = await response.json();
-      console.log('from useEffect2 objView=', objforview);
-
-      SetObjView({
-        id: id,       
-        objdata: objforview,
-      })
+      SetLoading(true);
+      try {
+        let id = objView.id;
+        let url = `https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${id}.json`;
+        const response = await fetch(url);
+        if (!response.ok) { throw new Error(response.statusText) }
+        else {
+          const objforview = await response.json();
+          SetObjView({
+            id: id,
+            objdata: objforview,
+          })        
+        }      
+      }
+      catch (e) { console.error(e) }
+      finally { SetLoading(false) }
     }
-    try {
-      fetchData();
-    }
-    catch (e) { console.error(e) }
-  }, [objView.id])
+    fetchData();
+  }, [objView.id]) 
 
+
+  function updateStates(e, id) {
+    updateObjView(e, id);
+    updateData(e, id);
+  }
+
+  const updateData = (e, id) => {
+    e.preventDefault();
+    let updatedData = [{
+      id: '',
+      name: '',
+      active: false
+    }];
+
+    let indid = data.findIndex(obj => obj.id === id);
+    let indtrue = data.findIndex(obj => obj.active === true);
+    updatedData = [...data];
+    if (indtrue < 0) {
+      updatedData[indid] = { id: data[indid].id, name: data[indid].name, active: true }
+    }
+    else {
+      updatedData[indtrue] = { id: data[indtrue].id, name: data[indtrue].name, active: false };
+      updatedData[indid] = { id: data[indid].id, name: data[indid].name, active: true };
+    }
+    SetData(updatedData);
+  }
+
+  const updateObjView = (e, id) => {
+    e.preventDefault();
+    SetObjView((prevObjView) => {
+      if (prevObjView.id === id) {
+        return prevObjView
+      }
+      else
+        return {
+          id: id,
+          objdata: '',          
+        }
+    })
+  }
 
   const handleClick = async (e, id) => {
-    e.preventDefault();
-    e.currentTarget.style.backgroundColor = 'salmon';
-    e.currentTarget.style.color = 'white';
-    SetObjView({
-      id: id,
-      loading: true,
-      objdata: '',
-    })
+    e.preventDefault();    
+    updateStates(e, id);
+  }
 
-  }  
-
-  if (objView.id === '') {  
-    return <List onClick={handleClick} data={data} />     
+  if (objView.objdata === '') {
+    return <List onClick={handleClick} data={data} />
   }
   else {
     return (
       <div className='container'>
-        <List onClick={handleClick} data={data} />
-        <Details obj={objView.objdata} />
+        <List  onClick={handleClick} data={data} />        
+        <Details obj={objView.objdata} loading={loading} />
       </div>
     )
   };
 }
 
 export default App;
+
+
--- src/components/List.js
+++ src/components/List.js
@@ -2,20 +2,25 @@ import React from 'react';
 
 
 export default function List(props) {
-    const { data, onClick } = props;  
-    console.log('from list data=', data);
+    const { data, onClick } = props;      
 
-    return (
-        <table className='table' style={{width: "30%", height: 'auto'}}>
-            <tbody>
-            {
-                data.map((obj) => {                            
-                <tr key={obj.id}>
-                    <td className='table-cells' onClick={(e) => onClick(e, obj.id)}>{obj.name}</td>
+    return (     
+        <table className='table'>
+        <tbody>  
+            {  
+            data.map((item)=> {
+                return (
+                    <tr key={item.id}>
+                    <td className='table-cells' 
+                        onClick={(e) => onClick(e, item.id)}
+                        style={{backgroundColor: item.active?'blue':null,
+                                color: item.active?'white':'black'}}>{item.name}</td>
                 </tr>
-                })
-            }
-            </tbody>
-        </table>
+                )
+            })                     
+           
+}          
+        </tbody>
+    </table>
     )
 }
